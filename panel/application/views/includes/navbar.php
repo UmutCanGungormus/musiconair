e1b064e74a8005bfb32f87b9b3cba19ea49acbbf
mutfakyapimemre <emrekilic@mutfakyapim.com>
2020-07-23 22:01:50 +0300

    removed images

--- panel/application/views/includes/navbar.php
+++ panel/application/views/includes/navbar.php
@@ -16,38 +16,6 @@
                         <div class="media">
                             <div class="media-img-wrap">
                                 <div class="avatar avatar-sm">
-                                    <img src="" alt="user" class="avatar-img rounded-circle">
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <div>
-                                    <div class="notifications-text"><span class="text-dark text-capitalize">Evie Ono</span> accepted your invitation to join the team</div>
-                                    <div class="notifications-time">12m</div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="javascript:void(0);" class="dropdown-item">
-                        <div class="media">
-                            <div class="media-img-wrap">
-                                <div class="avatar avatar-sm">
-                                    <img src="#" alt="user" class="avatar-img rounded-circle">
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <div>
-                                    <div class="notifications-text">New message received from <span class="text-dark text-capitalize">Misuko Heid</span></div>
-                                    <div class="notifications-time">1h</div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="javascript:void(0);" class="dropdown-item">
-                        <div class="media">
-                            <div class="media-img-wrap">
-                                <div class="avatar avatar-sm">
                                     <span class="avatar-text avatar-text-primary rounded-circle">
                                         <span class="initial-wrap"><span><i class="fas fa-user font-18"></i></span></span>
                                     </span>
@@ -101,19 +69,7 @@
             </div>
         </li>
         <li class="nav-item dropdown dropdown-authentication">
-            <a class="nav-link dropdown-toggle no-caret" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <div class="media">
-                    <div class="media-img-wrap">
-                        <div class="avatar">
-                            <img src="<?=base_url("assets/img/avatar12.jpg")?>" alt="<?= $user->full_name; ?>" class="avatar-img rounded-circle">
-                        </div>
-                        <span class="badge badge-success badge-indicator"></span>
-                    </div>
-                    <div class="media-body">
-                        <span><?= $user->full_name; ?><i class="fa fa-chevron-down"></i></span>
-                    </div>
-                </div>
-            </a>
+            <a class="nav-link dropdown-toggle no-caret" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i><span class="badge-wrap"><span class="badge badge-success badge-indicator badge-indicator-sm badge-pill pulse ml-1 mr-1"></span></span><small><?= $user->full_name; ?> <i class="ml-1 fa fa-chevron-down"></i></small></a>
             <div class="dropdown-menu dropdown-menu-right" data-dropdown-in="flipInX" data-dropdown-out="flipOutX">
                 <a class="dropdown-item" href="<?= base_url("users/update_form/$user->id"); ?>"><i class="dropdown-icon fa fa-user"></i><span>Profilim</span></a>
                 <div class="dropdown-divider"></div>
